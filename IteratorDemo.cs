b18cf29ec429441ce2533ca39c697049c809b54d
Vishakha Karimungi <Vishakha.Karimungi@mindbodyonline.com>
2021-08-18 09:31:11 +0530

    Add project files.

---
+++ IteratorDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeyWorld
+{
+    class IteratorDemo :IEnumerable
+    {
+        string[] movies = { "mov1", "mov2", "mov3" ,"mov4"};
+        string[,] reviws = new string[4, 2]
+        {
+            {"4","good" },
+            {"4","vgood" },
+            {"4","ok" },
+            {"4","bad" }
+        };
+        public IEnumerator GetEnumerator()
+        {
+            for(int i=0;i<movies.Length;i++)
+            {
+                yield return movies[i];
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace HeyWorld
+{
+    interface IPerson
+    {
+        void displayinfo();
+        void acceptinfo(string name, int id, string city);
+    }
+   public  abstract class Person
+    {
+        public string name;
+        public int id;
+        public string city;
+        abstract public void displayinfo();
+
+        public void acceptinfo(string name, int id, string city)
+        {
+            this.name = name;
+            this.id = id;
+            this.city = city;
+        }
+    }
+    public class Buyer : Person
+    {
+        public Buyer()
+        {
+            Console.WriteLine("Buyer constructor");
+        }
+        public override void displayinfo()
+        {
+            Console.WriteLine("Inside Buyer class " + name + " " + id + " " + city);
+        }
+
+    }
+    public class Customer : Buyer
+    {
+        public Customer()
+        {
+            Console.WriteLine("customer constructor");
+        }
+        public new void displayinfo()
+        {
+            Console.WriteLine("using new keyword");
+            Console.WriteLine("Inside customer class " + name + " " + id + " " + city);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Customer customer = new Customer();
+            customer.acceptinfo("vishakha", 1, "satara");
+            customer.displayinfo();
+
+            Buyer buyer = new Buyer();
+            buyer.acceptinfo("Buyer1", 2, "pune");
+            buyer.displayinfo();*/
+            IteratorDemo iter = new IteratorDemo();
+           foreach(string s in iter)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
